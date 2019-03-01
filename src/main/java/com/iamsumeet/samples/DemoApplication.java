63db93e762254ce047eaacfbe782da0252beec81
sumeet_joy <sumeet.joy@ascendcorp.com>
2019-03-01 13:11:35 +0700

    Added HTTPs

--- src/main/java/com/iamsumeet/samples/DemoApplication.java
+++ src/main/java/com/iamsumeet/samples/DemoApplication.java
@@ -2,6 +2,10 @@ package com.iamsumeet.samples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,5 +14,14 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Configuration
+	@EnableWebSecurity
+	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.requiresChannel().anyRequest().requiresSecure();
+			http.httpBasic().disable();
+		}
+	}
 }
 
